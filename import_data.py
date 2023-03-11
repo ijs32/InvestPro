4c6f8b4774f7b8129a772abc0b96df7ce397eadc
ijs32 <iansilber32@gmail.com>
2023-03-10 19:37:16 -0500

    importing data

--- import_data.py
+++ import_data.py
@@ -1,16 +1,61 @@
-import requests, json, os
+import requests, random, os, csv, pandas as pd
 from dotenv import load_dotenv
 
-load_dotenv()
 
-api_key = os.getenv('API_KEY')
+def import_data():
+  load_dotenv('./.env')
 
-url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&symbol=IBM&apikey={api_key}'
-url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey={api_key}'
-url = f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol=IBM&apikey={api_key}'
-url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey={api_key}'
-response = requests.get(url)
-data = response.json()
+  api_keys = os.getenv('API_KEYS')
+  api_keys = api_keys.split(',')
 
+  df_data = pd.read_csv('./data/nasdaq_symbols.csv')
 
-print(json.dumps(data, indent=4, sort_keys=True))
+  remove = []
+
+  for i, tckr in enumerate(df_data['Symbol']):
+    
+    api_key = random.choice(api_keys)
+
+    try: 
+      url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={tckr}&apikey={api_key}'
+      response = requests.get(url)
+      if response != {}:
+        print(response.json())
+        bal_sheet = response.json()['quarterlyReports']
+      
+      url = f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol={tckr}&apikey={api_key}'
+      response = requests.get(url)
+      if response != {}:
+        print(response.json())
+        cash_flow = response.json()['quarterlyReports']
+
+      url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={tckr}&apikey={api_key}'
+      response = requests.get(url)
+      if response != {}:
+        print(response.json())
+        income_statement = response.json()['quarterlyReports']
+
+      remove.append(i)
+
+      for quarter in {**{"ticker": tckr}, **bal_sheet, **cash_flow, **income_statement}:
+        print(quarter)
+
+        with open('./data/company_statements.csv', 'a', newline='') as csvfile:
+          # create a writer object
+          writer = csv.writer(csvfile)
+
+          # write the data rows
+          writer.writerow(quarter.values())
+        
+    except:
+      print('API limit reached')
+      for index in sorted(remove, reverse=True):
+        df_data = df_data.drop(index)
+
+      df_data.to_csv('./data/nasdaq_symbols.csv', index=False)
+      break
+
+    break
+# url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&symbol={tckr}&apikey={api_key}'
+# response = requests.get(url)
+# data = response.json()
---
+++ timer.py
@@ -0,0 +1,8 @@
+import time
+from import_data import import_data
+
+while True:
+  # sleep for 5 minutes
+  import_data()
+  time.sleep(300)
+
